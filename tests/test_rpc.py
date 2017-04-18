b2603cc45df018aede3e95b86cd8b8c47e7d3315
Benjamin Dopplinger <b.dopplinger@gmail.com>
2017-04-18 23:02:00 +1000

    Add sample test which allows validation

--- tests/test_rpc.py
+++ tests/test_rpc.py
@@ -73,3 +73,51 @@ def test_receiveItemBarCode():
     server = NullServer(
         Application([rpc.receiveItemBarCodeService], 'some_tns'))
     server.service.Flow_Services_Custom_receiveItemBarCode(obj)
+
+def test_xml_checker():
+    from io import BytesIO
+
+    from lxml import etree
+    from lxml.doctestcompare import LXMLOutputChecker, PARSE_XML
+    from spyne import srpc, Array, Integer, ServiceBase
+    from spyne.protocol.soap import Soap11
+    from spyne.server.wsgi import WsgiApplication
+
+    class SomeService(ServiceBase):
+        @srpc(Integer, _body_style='bare', _returns=Array(Integer))
+        def some_call(foo):
+            return [10] * foo
+
+    req = b"""
+    <soap11env:Envelope  xmlns:soap11env="http://schemas.xmlsoap.org/soap/envelope/"
+                    xmlns:tns="tns">
+        <soap11env:Body>
+            <tns:some_call>2</tns:some_call>
+        </soap11env:Body>
+    </soap11env:Envelope>
+    """
+
+    app = Application([SomeService], 'tns', in_protocol=Soap11(validator='lxml'), out_protocol=Soap11(validator='lxml'))
+    server = WsgiApplication(app)
+    response = etree.fromstring(b''.join(server({
+        'QUERY_STRING': '',
+        'PATH_INFO': '/call',
+        'REQUEST_METHOD': 'POST',
+        'CONTENT_TYPE': 'text/xml; charset=utf8',
+        'wsgi.input': BytesIO(req)
+    }, lambda a, b: None, "http://null")))
+
+    response_str = etree.tostring(response, pretty_print=True)
+    print(response_str)
+
+    expected = b"""
+        <soap11env:Envelope xmlns:soap11env="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tns="tns">
+          <soap11env:Body>
+            <tns:some_callResponse>
+              <tns:integer>10</tns:integer>
+              <tns:integer>10</tns:integer>
+            </tns:some_callResponse>
+          </soap11env:Body>
+        </soap11env:Envelope>"""
+    if not LXMLOutputChecker().check_output(expected, response_str, PARSE_XML):
+        raise Exception("Got: %s but expected: %s" % (response_str, expected))
